87efcf797b3c94815565f06a7ba4d766bc916c2d
termux-hackr <73882037+termux-hackr@users.noreply.github.com>
2020-11-13 13:57:18 +0500

    Add files via upload

---
+++ emoji.py
@@ -0,0 +1,17 @@
+import emoji
+
+print(emoji.emojize(":grinning_face_with_big_eyes:"))
+
+print(emoji.emojize(":winking_face_with_tongue:"))
+
+print(emoji.emojize(":zipper-mouth_face:"))
+
+print(emoji.emojize(":grinning_face_with_smiling_eyes:"))
+
+print(emoji.emojize(":beaming_face_with_smiling_eyes:"))
+
+print(emoji.emojize(":grinning_squinting_face:"))
+
+print(emoji.emojize(":grinning_face_with_sweat:"))
+
+print(emoji.emojize(":rolling_on_the_floor_laughing:"))
